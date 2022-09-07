bd00caa9d535d054ac217a757d0cf8975c7e065c
ABeer905 <arbeers@wisc.edu>
2022-09-07 14:54:14 -0500

    Playlist management

--- src/app.js
+++ src/app.js
@@ -1,28 +1,81 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require("electron")
+const path = require("path")
+const fs = require("fs")
 
-function createWindow () {
-  const win = new BrowserWindow({
-    minWidth: 900,
-    minHeight: 600,
-    autoHideMenuBar: true,
-  })
+const savePath = path.join(__dirname, "save.json")
+let fileLock = false
+let fileQueue = []
 
-  win.maximize()
-  win.loadFile(path.join("templates", "index.html"))
-  //win.webContents.openDevTools()
+function createWindow (save) {
+    const win = new BrowserWindow({
+        minWidth: 900,
+        minHeight: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    })
+
+    registerAPI(save)
+    win.maximize()
+    win.loadFile(path.join("templates", "index.html"))
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    let save;
+    if(fs.existsSync(savePath)){
+        save = require(savePath)
+    }else{
+        save = {
+            playlists: {}
+        }
+        write(save)
+    }
+    createWindow(save)
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
+
+const registerAPI = (save) => {
+    ipcMain.handle("getAllPlaylists", () => save.playlists)
+    ipcMain.handle("createPlaylist", (e, args) => {
+        if(save.playlists.hasOwnProperty(args[0])) return null
+        save.playlists[args[0]] = {
+            thumbnail: args[1]
+        }
+        write(save)
+        return save.playlists[args[0]]
+    })
+    ipcMain.handle("editPlaylist", (e, args) => {
+        if(save.playlists.hasOwnProperty(args[1])) return null
+        save.playlists[args[1]] = save.playlists[args[0]]
+        delete save.playlists[args[0]]
+        write(save)
+    })
+    ipcMain.handle("deletePlaylist", (e, name) => {
+        delete save.playlists[name]
+        write(save)
+    })
+}
+
+const write = (save) => {
+    if(!fileLock){
+        fileLock = true
+        fs.writeFile(savePath, JSON.stringify(save), e => {
+            if(e) console.error(e)
+            fileLock = false
+            if(fileQueue.length > 0) write(fileQueue.pop())
+        })
+    }else{
+        fileQueue.push(save)
+    }
+}
--- static/utils.js
+++ static/utils.js
@@ -1,6 +1,52 @@
+//Initializing calls
+const createPlaylistModal = new bootstrap.Modal(document.getElementById('addPlaylist'))
+const delPlaylistModal = new bootstrap.Modal(document.getElementById('delPlaylist'))
+
+window.playlist.getAll()
+
 const resize = () => {
     container.style.height = `${window.innerHeight - nav.getBoundingClientRect().height}px`
 }
 resize()
+
+//Event listeners
 window.addEventListener("resize", () => resize())
+
+addPlaylist.addEventListener("hidden.bs.modal", () => {
+    addName.value = ""
+    addImage.value = ""
+})
+
+addPlaylist.addEventListener("show.bs.modal", () => document.getElementById("missing-alert").style.visibility = "hidden")
+addPlaylist.addEventListener("shown.bs.modal", () => addName.focus())
+
+//Helper functions
+const createPlaylist = () => {
+    if(addName.value == "" || addImage.value == "") {
+        document.getElementById("missing-field").innerText = addName.value == "" ? "name" : "image"
+        document.getElementById("missing-alert").style.visibility = "visible"
+        return
+    }
+    window.playlist.create(addName.value, addImage.files[0].path)
+    createPlaylistModal.hide()
+}
+
+const editPlaylist = (name) => {
+    const e = document.getElementById(`${name}-edit`)
+    e.readOnly = false
+    e.focus()
+}
+
+const delPlaylist = (name) => {
+    document.getElementById("confirm-delete").innerText = name
+    delPlaylistModal.show()
+}
+
+const confirmDelete = () => {
+    delPlaylistModal.hide()
+    const name = document.getElementById("confirm-delete").innerText
+    const e = document.getElementById(`${name}-playlist`)
+    e.remove()
+    window.playlist.delete(name)
+}
 
